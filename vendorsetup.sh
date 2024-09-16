18a551fb6e819dc151944b3caa8d7e61b1b27a02
Pvawan <150278745+Pvawan@users.noreply.github.com>
2024-09-17 04:39:30 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_m32-user
-add_lunch_combo omni_m32-userdebug
-add_lunch_combo omni_m32-eng
+add_lunch_combo twrp_m32-user
+add_lunch_combo twrp_m32-userdebug
+add_lunch_combo twrp_m32-eng
