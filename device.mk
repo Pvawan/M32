cd308a91900796f3e0f733187a89d8cb50a16b8f
Pvawan <150278745+Pvawan@users.noreply.github.com>
2024-09-17 04:34:52 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,10 @@
 #
 
 LOCAL_PATH := device/samsung/m32
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
